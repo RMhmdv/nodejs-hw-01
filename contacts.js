1b0c56e666f8a53a85fe2bc675e0f4343ff5262d
Ravshan Mahmudov <2321772@gmail.com>
2021-05-24 22:23:59 +0300

    app after test

--- contacts.js
+++ contacts.js
@@ -1,20 +1,44 @@
-// contacts.js
+const path = require('path');
+const fs = require("fs/promises");
+const { v4: uuidv4 } = require('uuid');
 
-const contactsPath = ;
-
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
 function listContacts() {
-  // ...твой код
+  fs.readFile(contactsPath, 'utf-8')
+    .then(JSON.parse)
+    .then(console.table)
+    .catch(err => console.log(err));
 }
 
 function getContactById(contactId) {
-  // ...твой код
+  fs.readFile(contactsPath, 'utf-8')
+    .then(JSON.parse)
+    .then(data => data.find(el => String(el.id) === String(contactId)))
+    .then(console.log)
+    .catch(err => console.log(err));
 }
 
 function removeContact(contactId) {
-  // ...твой код
+  fs.readFile(contactsPath, 'utf-8')
+    .then(JSON.parse)
+    .then(async data => {
+      const contacs = data.filter(el => String(el.id) !== String(contactId));
+      await fs.writeFile(contactsPath, JSON.stringify(contacs, null, 2));
+    })
+    .catch(err => console.log(err));
 }
 
 function addContact(name, email, phone) {
-  // ...твой код
+  const id = uuidv4();
+  const newContact = { id, name, email, phone };
+
+  (async () => {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const content = JSON.parse(data);
+    content.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(content, null, 2));
+    console.table(content);
+  })();
 }
+module.exports = { listContacts, getContactById, removeContact, addContact };
